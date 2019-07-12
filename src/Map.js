5b78f4f708291c0f12ad61ea93b06b4b24b7f081
Janette Chiu <janettechiu@gmail.com>
2019-07-13 09:44:36 +1000

    Make map in Sydney

--- src/Map.js
+++ src/Map.js
@@ -3,12 +3,29 @@ import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
 import './Map.css';
 
 class Map extends Component {
+  // Canva
+  markerPos1 = {
+    lat: -33.885149,
+    lng: 151.211213
+  }
+
+  // Prince Alfred Park
+  markerPos2 = {
+    lat: -33.887287,
+    lng: 151.205934,
+  }
+
+  markerPos3 = {
+    lat: -33.8548157,
+    lng: 151.2164539,
+  }
+
   constructor() {
     super()
     this.state = {
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      lat: -33.873146,
+      lng: 151.208231,
+      zoom: 14
     }
   }
 
@@ -20,7 +37,17 @@ class Map extends Component {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        <Marker position={position}>
+        <Marker position={this.markerPos1}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={this.markerPos2}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={this.markerPos3}>
           <Popup>
             A pretty CSS3 popup. <br/> Easily customizable.
           </Popup>
