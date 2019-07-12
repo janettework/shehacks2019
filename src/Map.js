8565acca6a53aae8ad854b49f65e22eb1e60f427
Janette Chiu <janettechiu@gmail.com>
2019-07-13 00:27:38 +1000

    Use react-leaflet and put a sample map on the page

---
+++ src/Map.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
+import './Map.css';
+
+class Map extends Component {
+  constructor() {
+    super()
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    }
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <LeafletMap center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
+}
+
+export default Map
