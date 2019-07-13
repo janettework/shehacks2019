1633beb3012f2c932cf873f49e26910b9e6d0a32
Sarah Lawrence <s.j.lawrence8@gmail.com>
2019-07-13 14:31:14 +1000

    favourite tree

--- src/TreeInfo/TreeBar.js
+++ src/TreeInfo/TreeBar.js
@@ -5,11 +5,22 @@ import './index.css';
 
 export default class TreeBar extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      fave: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ fave: !this.state.fave });
+  }
 
   render() {
     return (
       <Pane display="flex" flexDirection="row" marginTop={20} marginBottom={16} justifyContent="center">
-        <Button iconBefore="heart" marginLeft={12} marginRight={12}>Fave Tree</Button>
+        <Button iconBefore="heart" marginLeft={12} marginRight={12} onClick={this.handleClick} intent={this.state.fave ? "danger" : "none"}>Favourite Tree</Button>
         <Button iconBefore="camera" marginLeft={12} marginRight={12}>Upload Photo</Button>
       </Pane>
     );
